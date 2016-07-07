08b3c468df3bc247c5ae9aca25d44fa12d6af7b0
Piotr Podusowski <podusowski@gmail.com>
2016-07-07 13:45:22 +0200

    pass params to train view

--- training/urls.py
+++ training/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url('^$', views.index, name='index'),
-    url('^train$', views.train, name='train'),
+    url('^train/(?P<training_session_id>[0-9]+)/$', views.train, name='train'),
 ]
--- training/views.py
+++ training/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import TrainingSession
+
 def index(request):
     return render(request, 'training/index.html')
 
-def train(request):
+def train(request, training_session_id):
+    s = TrainingSession.objects.get(pk=training_session_id)
     return render(request, 'training/train.html')
