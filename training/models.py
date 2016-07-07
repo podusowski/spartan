d854626b0b92b92d5e7f1d0e3429c94e24600be0
Piotr Podusowski <podusowski@gmail.com>
2016-07-07 12:26:04 +0200

    add foreign key

--- training/models.py
+++ training/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class TrainingSession(models.Model):
+    pass
+
 class Excercise(models.Model):
+    training_session = models.ForeignKey(TrainingSession)
     name = models.CharField(max_length=200)
     sets = models.CharField(max_length=200)
