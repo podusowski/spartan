b4cc80814d45ae0ac2523d690ed66ff6b7893c70
Piotr Podusowski <podusowski@gmail.com>
2016-07-08 11:29:58 +0200

    utd string, mainly to save to sites like garmin

--- training/models.py
+++ training/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class TrainingSession(models.Model):
-    pass
+    def utd(self):
+        """ userfriendly training data string """
+        return '\n'.join(map(lambda x: x.utd(), self.excercise_set.all()))
 
 
 class Excercise(models.Model):
+    def utd(self):
+        """ userfriendly training data string """
+        return ': '.join([self.name, self.sets])
+
     training_session = models.ForeignKey(TrainingSession)
     name = models.CharField(max_length=200)
     sets = models.CharField(max_length=200)
