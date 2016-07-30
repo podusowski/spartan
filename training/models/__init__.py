f459ab55b142573f62421e4ac0fbbb70c25de99c
Piotr Podusowski <podusowski@gmail.com>
2016-07-30 13:53:06 -0400

    gps model draft

--- training/models/__init__.py
+++ training/models/__init__.py
@@ -1,1 +1,2 @@
 from .strength import *
+from .gpx import *
---
+++ training/models/gpx.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models import Count
+from django.contrib.auth.models import User
+from django.template import defaultfilters
+from django.db.models import Sum
+
+class GpxWorkout(models.Model):
+    user = models.ForeignKey(User)
