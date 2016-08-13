2b403c21eb1c1b2f01be12f9c10f2fce68e32b38
Piotr Podusowski <podusowski@gmail.com>
2016-08-13 03:09:18 -0400

    don't crash when you're not running

--- training/units.py
+++ training/units.py
@@ -5,7 +5,9 @@ def mpkm_from_mps(m_per_s):
         return '-'
 
 def km_from_m(m):
-    if m > 1000:
+    if m is None:
+        return 0
+    elif m > 1000:
         return '{}km'.format(round(m / 1000, 2))
     else:
         return '{}m'.format(m)
