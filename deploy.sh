f23c8295421d7c94fe74bd592eb99440d7ce793c
Piotr Podusowski <podusowski@gmail.com>
2016-08-14 06:34:27 -0400

    deploy script refactor

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,56 @@
-if [ `id -u` -ne 0 ]; then
-    echo you should be root to do this
-    exit 1
-fi
+function expect_root()
+{
+    if [ `id -u` -ne 0 ]; then
+        echo you should be root to do this
+        exit 1
+    fi
+}
 
-echo make sure what that script is doing before continuing
-echo hit enter or ^c to abort
-
-read line
-
-if ! pwd | grep '/var/www'; then
-    echo 'it seems you are outside apache directory (at least as far as I can say), hit enter if you are sure if this is the right place'
+function print_warning()
+{
+    echo make sure what that script is doing before continuing
+    echo hit enter or ^c to abort
     read line
+}
+
+function expect_www_wd()
+{
+    if ! pwd | grep '/var/www'; then
+        echo 'it seems you are outside apache directory (at least as far as I can say), hit enter if you are sure if this is the right place'
+        read line
+    fi
+}
+
+function pull_www()
+{
+    expect_root
+    expect_www_wd
+
+    service apache2 stop
+    sudo -u www-data bash -c 'git pull'
+    sudo -u www-data bash -c './manage.py migrate'
+    sudo -u www-data bash -c './manage.py collectstatic'
+    service apache2 start
+}
+
+function apt()
+{
+    echo
+}
+
+function help()
+{
+    grep '^function .*()' $0
+}
+
+actions=$@
+
+if [ -z "$actions" ]; then
+    actions="print_warning pull_www"
 fi
 
-service apache2 stop
-sudo -u www-data bash -c 'git pull'
-sudo -u www-data bash -c './manage.py migrate'
-sudo -u www-data bash -c './manage.py collectstatic'
-service apache2 start
+for action in $actions; do
+    echo "*** $action"
+    $action
+    echo "***"
+done
