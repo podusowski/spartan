8661c64594c79f49ee9c12d25578a34cd381bc55
Piotr Podusowski <podusowski@gmail.com>
2016-08-15 06:00:20 -0400

    rewrite

--- deploy.sh
+++ deploy.sh
@@ -1,71 +1,31 @@
 #!/bin/bash
 
-function expect_root()
-{
-    if [ `id -u` -ne 0 ]; then
-        echo you should be root to do this
-        exit 1
-    fi
-}
-
-function print_warning()
-{
-    echo make sure what that script is doing before continuing
-    echo hit enter or ^c to abort
-    read line
-}
-
-function expect_www_wd()
-{
-    if ! pwd | grep '/var/www'; then
-        echo "it seems you are outside apache directory"
-        exit 1
-    fi
-}
-
-function update_apache()
-{
-    cp 000-default.conf /etc/apache2/sites-available/
-}
-
-function pull_www()
-{
-    expect_root
-    expect_www_wd
-
-    service apache2 stop
-    sudo -E -u www-data bash -c ". env/bin/activate &&\
-                                 git pull &&\
-                                 ./manage.py migrate &&\
-                                 rm static/ -rf &&\
-                                 ./manage.py collectstatic --noinput"
-
-    update_apache
-
-    service apache2 start
-}
-
-function update_virtualenv()
-{
-    pip install -r requirements.txt
-}
-
-function help()
-{
-    grep '^function .*()' $0
-}
-
-# this wont work inside a function
-. env/bin/activate
-
-actions=$@
-
-if [ -z "$actions" ]; then
-    actions="pull_www update_virtualenv"
+# asserts
+if [ `id -u` -ne 0 ]; then
+    echo you should be root to do this
+    exit 1
 fi
 
-for action in $actions; do
-    echo "*** $action"
-    $action
-    echo "***"
-done
+if ! pwd | grep '/var/www'; then
+    echo "it seems you are outside apache directory"
+    exit 1
+fi
+
+service apache2 stop
+
+if [ ! -e env ]; then
+    virtualenv env
+else
+
+. env/bin/activate
+
+sudo -E -u www-data bash -c ". env/bin/activate &&\
+                             git pull &&\
+                             ./manage.py migrate &&\
+                             rm static/ -rf &&\
+                             ./manage.py collectstatic --noinput"
+
+cp 000-default.conf /etc/apache2/sites-available/
+pip install -r requirements.txt
+
+service apache2 start
