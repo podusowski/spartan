8e37dbbc34c47e24b60a75fb014fe84771ef1de7
Piotr Podusowski <podusowski@gmail.com>
2016-08-28 13:29:53 -0400

    reqs for postgres

--- spartan/settings.py
+++ spartan/settings.py
@@ -56,13 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'spartan.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
