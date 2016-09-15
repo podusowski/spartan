a032dc4dc398e7d4efd7df9120fd7b69405ad077
Piotr Podusowski <podusowski@gmail.com>
2016-09-15 14:24:37 -0400

    don't create pyc

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.dont_write_bytecode = True
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spartan.settings")
 
     from django.core.management import execute_from_command_line
