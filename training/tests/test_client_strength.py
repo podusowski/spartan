2a6f4fcb06ee01d7cc377ca7c7eaab2d6254e426
Piotr Podusowski <podusowski@gmail.com>
2016-09-22 09:38:38 -0400

    404 support in workout view

--- training/tests/test_client_strength.py
+++ training/tests/test_client_strength.py
@@ -9,17 +9,14 @@ class ClienStrengthTestCase(TestCase):
         user = User.objects.create_user(username='grzegorz', email='', password='z')
         self.client = Client()
 
-    def _expect_accessible_workout_page(self, workout_id):
+    def _expect_workout_page(self, workout_id, status_code=200):
         response = self.client.get('/workout/{}'.format(workout_id), follow=True)
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(status_code, response.status_code)
 
     def test_create_workout(self):
         response = self.client.post('/login/', {'username': 'grzegorz', 'password': 'z'})
         self.assertEqual(302, response.status_code)
         self.assertEqual('/dashboard', response['Location'])
-
-        # no workouts yet
-        self.assertEqual(0, len(models.Workout.objects.all()))
 
         response = self.client.get('/start_workout', follow=True)
         self.assertEqual(200, response.status_code)
@@ -27,19 +24,19 @@ class ClienStrengthTestCase(TestCase):
 
         workout_id = response.context['workout'].id
 
-        self._expect_accessible_workout_page(workout_id)
+        self._expect_workout_page(workout_id)
 
         response = self.client.post('/add_excercise/{}/'.format(workout_id), {'name': 'push-up'}, follow=True)
         self.assertEqual(200, response.status_code)
 
-        self._expect_accessible_workout_page(workout_id)
+        self._expect_workout_page(workout_id)
 
         response = self.client.post('/finish_workout/{}'.format(workout_id), follow=True)
         self.assertEqual(200, response.status_code)
 
-        self._expect_accessible_workout_page(workout_id)
+        self._expect_workout_page(workout_id)
 
         response = self.client.post('/delete_workout/{}/'.format(workout_id), follow=True)
         self.assertEqual(200, response.status_code)
 
-        self.assertEqual(0, len(models.Workout.objects.all()))
+        self._expect_workout_page(workout_id, status_code=404)
