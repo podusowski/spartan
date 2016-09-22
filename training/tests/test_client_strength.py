eccbbeb88817e0316fc1b6c28428dc8bd405db84
Piotr Podusowski <podusowski@gmail.com>
2016-09-22 09:24:59 -0400

    client tests stuff

--- training/tests/test_client_strength.py
+++ training/tests/test_client_strength.py
@@ -5,32 +5,41 @@ from training import models
 
 
 class ClienStrengthTestCase(TestCase):
-    def test_create_workout(self):
+    def setUp(self):
         user = User.objects.create_user(username='grzegorz', email='', password='z')
+        self.client = Client()
 
-        c = Client()
-        response = c.post('/login/', {'username': 'grzegorz', 'password': 'z'})
+    def _expect_accessible_workout_page(self, workout_id):
+        response = self.client.get('/workout/{}'.format(workout_id), follow=True)
+        self.assertEqual(200, response.status_code)
+
+    def test_create_workout(self):
+        response = self.client.post('/login/', {'username': 'grzegorz', 'password': 'z'})
         self.assertEqual(302, response.status_code)
         self.assertEqual('/dashboard', response['Location'])
 
         # no workouts yet
         self.assertEqual(0, len(models.Workout.objects.all()))
 
-        response = c.get('/start_workout', follow=True)
+        response = self.client.get('/start_workout', follow=True)
         self.assertEqual(200, response.status_code)
         self.assertIsNotNone(response.context['workout'])
 
         workout_id = response.context['workout'].id
 
-        response = c.post('/add_excercise/{}/'.format(workout_id), {'name': 'push-up'}, follow=True)
+        self._expect_accessible_workout_page(workout_id)
+
+        response = self.client.post('/add_excercise/{}/'.format(workout_id), {'name': 'push-up'}, follow=True)
         self.assertEqual(200, response.status_code)
 
-        self.assertEqual(1, len(models.Workout.objects.all()))
+        self._expect_accessible_workout_page(workout_id)
 
-        response = c.post('/finish_workout/{}/'.format(workout_id), follow=True)
+        response = self.client.post('/finish_workout/{}'.format(workout_id), follow=True)
+        self.assertEqual(200, response.status_code)
 
-        self.assertEqual(1, len(models.Workout.objects.all()))
+        self._expect_accessible_workout_page(workout_id)
 
-        response = c.post('/delete_workout/{}/'.format(workout_id), follow=True)
+        response = self.client.post('/delete_workout/{}/'.format(workout_id), follow=True)
+        self.assertEqual(200, response.status_code)
 
         self.assertEqual(0, len(models.Workout.objects.all()))
