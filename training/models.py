a6e76654844cd33b7237cb7c9d46cbd3174fa055
Piotr Podusowski <podusowski@gmail.com>
2016-09-28 08:57:16 -0400

    format

--- training/models.py
+++ training/models.py
@@ -57,14 +57,14 @@ class Workout(models.Model):
 
         self.finished = timezone.now()
 
-    def _total_reps(self):
-        return Reps.objects.filter(excercise__workout=self).aggregate(Sum('reps'))['reps__sum'] or 0
-
     def duration(self):
         if self.started is not None and self.finished is not None:
             return self.finished - self.started
         else:
             return datetime.timedelta()
+
+    def _total_reps(self):
+        return Reps.objects.filter(excercise__workout=self).aggregate(Sum('reps'))['reps__sum'] or 0
 
     def _total_distance(self):
         ''' for gpx workouts '''
