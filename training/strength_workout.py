9abd2b773a3570087768e36db893d1770e1a4eab
Piotr Podusowski <podusowski@gmail.com>
2016-10-13 14:21:44 -0400

    extract function

--- training/strength_workout.py
+++ training/strength_workout.py
@@ -1,3 +1,5 @@
+import logging
+
 import django.utils.timezone
 
 from training import models
@@ -8,25 +10,27 @@ def start_workout(user):
     return workout.id
 
 
+def _finish_active_excercise(workout):
+    try:
+        current_excercise = workout.excercise_set.latest('pk')
+        current_excercise.time_finished = django.utils.timezone.now()
+        current_excercise.save()
+    except models.Excercise.DoesNotExist:
+        logging.debug("nothing to finish - no active excercise yet")
+
+
 def finish_workout(request, workout_id):
     workout = models.Workout.objects.get(pk=workout_id)
     workout.finish()
     workout.save()
 
-    current_excercise = workout.excercise_set.order_by('-pk')[0]
-    current_excercise.time_finished = django.utils.timezone.now()
-    current_excercise.save()
+    _finish_active_excercise(workout)
 
 
 def add_excercise(user, workout_id, name):
     workout = models.Workout.objects.get(pk=workout_id, user=user)
 
-    try:
-        current_excercise = workout.excercise_set.order_by('-pk')[0]
-        current_excercise.time_finished = django.utils.timezone.now()
-        current_excercise.save()
-    except:
-        pass
+    _finish_active_excercise(workout)
 
     excercise = workout.excercise_set.create(name=name)
     try:
