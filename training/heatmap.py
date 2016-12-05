a65af2cd4303d3916a3e1dfcf48ddcd3b2e418cf
Piotr Podusowski <podusowski@gmail.com>
2016-12-05 06:50:38 -0500

    misc

--- training/heatmap.py
+++ training/heatmap.py
@@ -21,13 +21,13 @@ def _dump(data):
     return json.dumps(data, default=json_encode_decimal)
 
 
-def web_mercator(point):
+def _web_mercator(point):
     lon, lat = point
     return pyproj.transform(EPSG4326, WEB_MERCATOR, lon, lat)
 
 
 def _process_points(activity):
-    points = map(web_mercator, activity)
+    points = map(_web_mercator, activity)
     points = map(hexagons.point_to_hexagon, points)
     points = set(points)
     points = list(points)
@@ -49,6 +49,9 @@ def generate_heatmap(user, days=None):
             activities[activity_type] = []
         activities[activity_type].append((lat, lon))
 
-    activities = [{'activity_type': activity_type, 'points': _process_points(points)} for activity_type, points in activities.items()]
+    activities = [{'activity_type': activity_type,
+                   'points': _process_points(points)} for activity_type, points in activities.items()]
 
-    return {'total_points': len(points), 'json': _dump(activities), 'activities': activities}
+    return {'total_points': len(points),
+            'json': _dump(activities),
+            'activities': activities}
