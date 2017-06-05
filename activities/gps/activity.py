35183c82afc58de27621b4942a80ee1c5458529d
Piotr Podusowski <podusowski@gmail.com>
2017-06-05 21:51:11 +0200

    not used function removed

--- activities/gps/activity.py
+++ activities/gps/activity.py
@@ -6,9 +6,5 @@ from training import models
 TYPE = 'gps'
 
 
-def supported(workout):
-    return workout.gpx_set.count() > 0
-
-
 def redirect_to_workout(workout):
     return redirect('show_gps_workout', workout.id)
