8d6e79788b5b543ea6029a4511bcf356ff8ac60c
Piotr Podusowski <podusowski@gmail.com>
2017-07-22 08:55:37 +0200

    fix in urls

--- activities/strength/urls.py
+++ activities/strength/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url('^workout/(?P<workout_id>[0-9]+)/$', views.workout, 'show_strength_workout'),
+    url('^workout/(?P<workout_id>[0-9]+)/$', views.workout, name='show_strength_workout'),
     url('^start_workout/$', views.start_workout, name='start_workout'),
     url('^finish_workout/(?P<training_session_id>[0-9]+)$', views.finish_workout, name='finish_workout'),
     url('^add_excercise/(?P<training_session_id>[0-9]+)/$', views.add_excercise, name='add_excercise'),
