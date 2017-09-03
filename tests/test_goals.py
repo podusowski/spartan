40d9d49868de599e14ccf9f3c3a9aa3d646ab118
Piotr Podusowski <podusowski@gmail.com>
2017-09-03 17:17:50 +0200

    some goal coverage in top-level tests

---
+++ tests/test_goals.py
@@ -0,0 +1,39 @@
+from tests.utils import *
+from tests import utils
+from training import units
+
+from tests import test_strength
+
+
+class GoalsTestCase(ClientTestCase):
+    _strength_workout = utils.strength_workout
+
+    def _set_goal(self, name, volume):
+        return self.post("/statistics/add_goal", {'name': name, 'volume': volume})
+
+    def test_create_goal(self):
+        workout_statistics_page = self._set_goal("push-up", 100)
+
+        goal = workout_statistics_page.context["goal"]
+        assert 100 == goal.volume
+        assert units.Volume(0) == goal.progress
+
+    def test_both_user_sets_some_goals(self):
+        """
+        Make sure user get its own goals despite the fact that other user
+        created one for itself.
+        """
+
+        self._set_goal("push-up", 100)
+
+        self.switch_user(self.other_user)
+
+        self._set_goal("push-up", 50)
+
+        self.switch_user(self.user)
+
+        workout_statistics_page = self.get("/statistics/workout/push-up")
+
+        goal = workout_statistics_page.context["goal"]
+        assert 100 == goal.volume
+        assert units.Volume(0) == goal.progress
