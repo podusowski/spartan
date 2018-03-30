36883e30b897bf1c3c19afdbb4667169c05c3faa
Piotr Podusowski <podusowski@gmail.com>
2018-03-30 23:06:37 +0200

    use gunicord + whitenoise

--- spartan/settings.py
+++ spartan/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'spartan.urls'
--- spartan/urls.py
+++ spartan/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import settings
 
@@ -11,4 +10,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
