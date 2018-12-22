cb36e5589aad1d075957b65cde0df3aa9547c8bf
Piotr Podusowski <podusowski@gmail.com>
2018-12-22 16:42:07 +0100

    migrations removed

--- spartan/settings.py
+++ spartan/settings.py
@@ -25,18 +25,27 @@ INSTALLED_APPS = (
     'compressor',
 )
 
-MIDDLEWARE_CLASSES = (
+    #'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.common.CommonMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'training.userprof.TimezoneMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'training.userprof.TimezoneMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
 
 ROOT_URLCONF = 'spartan.urls'
 
