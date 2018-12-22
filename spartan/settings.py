7e1730582852a7bf1042115601f55611c15c39bf
Piotr Podusowski <podusowski@gmail.com>
2018-12-22 16:48:41 +0100

    reenable whitenoise

--- spartan/settings.py
+++ spartan/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
